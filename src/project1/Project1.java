936299cb2a142bd5e598f49a7cae4aa70aed0510
antrohebsiba <antrohebsibaxe809@gmail.com>
2023-08-03 00:09:53 -1200

    app file

---
+++ src/project1/Project1.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author ELCOT
+ */
+public class Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //operators
+        //Comparsion or Relational operators
+       /* int a=43;
+        int b=55;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        
+        System.out.println("Welcome to the class");
+        System.out.print("Welome to");
+        System.out.print(" the class");*/
+       //get userInput
+       Scanner oh=new Scanner(System.in);
+       System.out.print("Enter your age:");
+       int age=oh.nextInt();
+       System.out.println(age+3);
+       
+        System.out.print("Enter PI value:");
+        float pi=oh.nextFloat();
+        System.out.println(pi);
+        
+        
+        
+        
+        
+        
+        
+       
+        
+        
+    
+        
+       
+        
+       
+       
+       
+       
+        
+        
+        
+       
+    }
+    
+}
